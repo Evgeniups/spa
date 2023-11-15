971c47c0b1644a16036c1bb1f2910c0f2f37619e
Evgeniups <evgeniups@gmail.com>
2023-11-15 18:17:29 +0200

    ssh

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import {NavLink} from 'react-router-dom';
 const Header = () => {
   return (
     <header className='header h-16 flex bg-center bg-sky-900 text-white items-center justify-center space-x-4 '>
-      <NavLink to='/'>Home1</NavLink>
+      <NavLink to='/'>Home</NavLink>
       <NavLink to='/about'>About</NavLink>
       <NavLink to='/contacts'>Contacts</NavLink>
       <NavLink to='/people'>People</NavLink>
