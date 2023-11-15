b206073a559d7010f8d7018f3b791d1a9dd43eb1
Eugene Skvortsov <evgeniups@gmail.com>
2023-11-15 19:00:53 +0200

    ssh

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className='header h-16 flex bg-center bg-sky-900 text-white items-center justify-center space-x-4 '>
       <NavLink to='/'>Home</NavLink>
-      <NavLink to='/about'>About1</NavLink>
+      <NavLink to='/about'>About2</NavLink>
       <NavLink to='/contacts'>Contacts</NavLink>
       <NavLink to='/people'>People</NavLink>
     </header>
