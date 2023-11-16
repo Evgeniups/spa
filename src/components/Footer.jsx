7abc2568e91fce1f169925d1e8d854a1cd630ea4
Eugene Skvortsov <evgeniups@gmail.com>
2023-11-16 09:48:15 +0200

    clean

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
-
 const Footer = () => {
   return (
     <footer className='footer h-10 flex bg-center bg-sky-900 text-white items-center justify-center space-x-4'>
-      All right reserved... Bla-bla-bla...
+      <span>© All right reserved... Bla-bla-bla...</span>
     </footer>
   );
 };
